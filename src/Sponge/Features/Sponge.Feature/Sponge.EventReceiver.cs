d5ade9adcf4e7902cf1781196bdf24c79312dbca
int32at <admin@int32.at>
2013-07-19 16:31:52 +0200

    added feature event receiver to create sponge web

---
+++ src/Sponge/Features/Sponge.Feature/Sponge.EventReceiver.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+using Microsoft.SharePoint;
+using Sponge.Common.Models;
+
+namespace Sponge.Feature
+{
+    [Guid("f1542a28-46d4-4d83-bd4e-4854d05bd787")]
+    public class EventReceiver : SPFeatureReceiver
+    {
+        public override void FeatureActivated(SPFeatureReceiverProperties properties)
+        {
+            var ca = SPWebManager.GetCentralAdminWeb();
+            using (var mgr = new SPManager(ca.Site))
+            {
+                var sponge = mgr.Webs.Create("Sponge Framework", "", "Sponge", "STS#1");
+            }
+        }
+
+        public override void FeatureUninstalling(SPFeatureReceiverProperties properties)
+        {
+            var ca = SPWebManager.GetCentralAdminWeb();
+            using (var mgr = new SPManager(ca.Site))
+            {
+                mgr.Webs.Delete("Sponge");
+            }
+        }
+    }
+}
