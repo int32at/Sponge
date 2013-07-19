db7e0c3e04101fb215089892fc465519b222bd0a
int32at <admin@int32.at>
2013-07-19 15:36:28 +0200

    added test solution

---
+++ src/Sponge.Tests/FeatureTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.SharePoint.Administration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sponge.Tests
+{
+    [TestClass]
+    public class FeatureTests
+    {
+        [TestMethod]
+        public void CreateWeb()
+        {
+            var adminWebApp = SPWebService.AdministrationService.WebApplications["SharePoint Central Administration v4"];
+        }
+    }
+}
