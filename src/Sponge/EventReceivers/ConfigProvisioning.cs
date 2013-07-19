d4c92dd30cdf7b8d919388307d01890c70d5c665
int32at <admin@int32.at>
2013-07-19 19:52:17 +0200

    added basic config provisioning event receiver

---
+++ src/Sponge/EventReceivers/ConfigProvisioning.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.SharePoint;
+
+namespace Sponge.EventReceivers
+{
+    public class ConfigProvisioning : SPItemEventReceiver
+    {
+        public override void ItemAdded(SPItemEventProperties properties)
+        {
+            base.ItemAdded(properties);
+        }
+
+        public override void ItemUpdated(SPItemEventProperties properties)
+        {
+            base.ItemUpdated(properties);
+        }
+
+        public override void ItemDeleted(SPItemEventProperties properties)
+        {
+            base.ItemDeleted(properties);
+        }
+    }
+}
