62d7d46806f3e686690aaecef9a565e9ff077e72
int32at <admin@int32.at>
2013-07-19 20:54:58 +0200

    added method to get the sponge web from ca

--- src/Sponge.Common/Utilities/Utils.cs
+++ src/Sponge.Common/Utilities/Utils.cs
@@ -1,13 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Microsoft.SharePoint;
-using Microsoft.SharePoint.Administration;
+﻿using Microsoft.SharePoint;
+using Sponge.Common.Models;
 
 namespace Sponge.Common.Utilities
 {
     public static class Utils
     {
+        public static SPWeb GetSpongeWeb()
+        {
+            using (var ca = SPWebManager.GetCentralAdminWeb())
+                return ca.Webs[Constants.SPONGE_WEB_URL];
+        }
     }
 }
