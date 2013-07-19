401522ee82d3fd8cb03881cad2468a0c4e03ae74
int32at <admin@int32.at>
2013-07-19 15:56:41 +0200

    added basic sp manager

---
+++ src/Sponge.Common/Models/SPManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.SharePoint;
+
+namespace Sponge.Common.Models
+{
+    public class SPManager : IDisposable
+    {
+        private SPSite _site;
+        private SPWeb _web;
+
+        public SPManager(string spSiteUrl)
+        {
+            _site = new SPSite(spSiteUrl);
+            _web = _site.RootWeb;
+        }
+        public SPManager(string spSiteUrl, string spwebUrl)
+        {
+            _site = new SPSite(spSiteUrl);
+            _web = _site.OpenWeb(spwebUrl); ;
+        }
+
+        public void Dispose()
+        {
+            if (_site != null)
+                _site.Dispose();
+
+            if (_web != null)
+                _web.Dispose();
+        }
+    }
+}
