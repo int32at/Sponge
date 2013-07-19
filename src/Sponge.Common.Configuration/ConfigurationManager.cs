2b3a941c6a7204072d8d831f9df7553d8727e0ac
int32at <admin@int32.at>
2013-07-19 22:47:34 +0200

    changed the config manager to use the Sponge Config lists instead of the persisted object

--- src/Sponge.Common.Configuration/ConfigurationManager.cs
+++ src/Sponge.Common.Configuration/ConfigurationManager.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.SharePoint;
+using Sponge.Common.Utilities;
+using Sponge.Common.Context;
 
 namespace Sponge.Common.Configuration
 {
@@ -10,46 +12,74 @@ namespace Sponge.Common.Configuration
     {
         public static T Get<T>(string app, string key)
         {
-            var conf = ConfigurationObject.Local;
+            var query = from item in Utils.Context.ConfigItems
+                        where item.Application.Title == app &&
+                              item.Title == key
+                        select item.Value;
 
-            if (!conf.Items.ContainsKey(app))
-                throw new Exception(string.Format("Application '{0}' not found.", app));
+            var conf = query.FirstOrDefault();
 
-            if(!conf.Items[app].Items.ContainsKey(key))
-                throw new Exception(string.Format("Key '{0}' not found.", key));
+            if (conf == null)
+                throw new Exception(string.Format("No Entry for Key '{0}' in Application '{1}' found.", key, app));
 
-            return (T)conf.Items[app].Items[key];
+            return (T)Convert.ChangeType(conf, typeof(T));
+        }
+
+        public static Dictionary<string, string> GetAll(string app)
+        {
+            var query = from item in Utils.Context.ConfigItems
+                        where item.Application.Title == app
+                        select new { Key = item.Title, Value = item.Value };
+
+            return query.ToDictionary(i => i.Key, i => i.Value);
         }
 
         public static void Set(string app, string key, object value)
         {
-            var conf = ConfigurationObject.Local;
+            var query = from item in Utils.Context.ConfigItems
+                        where item.Application.Title == app &&
+                              item.Title == key
+                        select item;
 
-            if (conf.Items.ContainsKey(app))
+            var conf = query.FirstOrDefault();
+
+            if (conf == null)
             {
-                if (conf.Items[app].Items.ContainsKey(key))
-                    conf.Items[app].Items[key] = value;
-                else
-                    conf.Items[app].Items.Add(key, value);
+                var appItem = Utils.Context.ConfigApplications.Where(i => i.Title == app).FirstOrDefault();
+
+                if (appItem == null)
+                    throw new Exception(string.Format("Application '{0}' not found"));
+
+                var item = new ConfigItemsItem()
+                {
+                    Application = appItem,
+                    Title = key,
+                    Value = value.ToString()
+                };
+
+                Utils.Context.ConfigItems.InsertOnSubmit(item);
             }
             else
             {
-                var item = new ConfigurationItem();
-                item.Items.Add(key, value);
-                conf.Items.Add(app, item);
+                conf.Value = value.ToString();
             }
 
-            conf.Update();
+            Utils.Context.SubmitChanges();
+        }
+        
+        public static void CreateApplication(string appName)
+        {
+            if (ApplicationExists(appName))
+                throw new Exception(string.Format("Application '{0}' already exists.", appName));
+
+            var app = new Item() { Title = appName };
+            Utils.Context.ConfigApplications.InsertOnSubmit(app);
+            Utils.Context.SubmitChanges();
         }
 
-        public static void Delete(string app)
+        public static bool ApplicationExists(string appName)
         {
-            var conf = ConfigurationObject.Local;
-
-            if (conf.Items.ContainsKey(app))
-                conf.Items.Remove(app);
-
-            conf.Update();
+            return Utils.Context.ConfigApplications.Where(i=>i.Title == appName).FirstOrDefault() != null;
         }
     }
 }
