cb7f8e8fe1adfd7b6f2773eed6407f8d3af170bc
int32at <admin@int32.at>
2013-07-19 23:17:14 +0200

    fixed typo; added Get support to show usage

--- scripts/add-config-entries.ps1
+++ scripts/add-config-entries.ps1
@@ -1,6 +1,6 @@
 #load sponge configuration assembly
 [System.Reflection.Assembly]::LoadWithPartialName('Sponge.Common.Configuration');
-write-host "Songe Configuration has been loaded."
+write-host "Sponge Configuration has been loaded."
 
 $applicationName = "Sponge Powershell App";
 
@@ -8,10 +8,13 @@ $applicationName = "Sponge Powershell App";
 [Sponge.Common.Configuration.ConfigurationManager]::CreateApplication($applicationName);
 write-host "Application '$applicationName' created"
 
-[Sponge.Common.Configuration.ConfigurationManager]::Set($applicationName, "SampleKey1", 4);
+[Sponge.Common.Configuration.ConfigurationManager]::Set($applicationName, "SampleKey1", 5);
 [Sponge.Common.Configuration.ConfigurationManager]::Set($applicationName, "SampleKey2", [guid]::NewGuid());
 [Sponge.Common.Configuration.ConfigurationManager]::Set($applicationName, "SampleKey3", 4.33);
 [Sponge.Common.Configuration.ConfigurationManager]::Set($applicationName, "SampleKey4", "admin@int32.at");
 [Sponge.Common.Configuration.ConfigurationManager]::Set($applicationName, "SampleKey5", "5431235§$%");
 
 write-host "Test Entries added."
+
+$result = [Sponge.Common.Configuration.ConfigurationManager]::Get($applicationName, "SampleKey2");
+write-host "SampleKey2 = $result";
