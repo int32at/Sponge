5b50e15b0f43ab381246aa8db504ae8cc0e85448
int32at <admin@int32.at>
2013-07-19 22:48:18 +0200

    added sponge sp context; added constants class with all used names. urls etc..

--- src/Sponge.Common/Utilities/Utils.cs
+++ src/Sponge.Common/Utilities/Utils.cs
@@ -1,14 +1,32 @@
 ﻿using Microsoft.SharePoint;
 using Sponge.Common.Models;
+using Sponge.Common.Context;
 
 namespace Sponge.Common.Utilities
 {
     public static class Utils
     {
+        private static SpongeContextDataContext _ctx;
+        public static SpongeContextDataContext Context
+        {
+            get
+            {
+                if (_ctx == null)
+                    _ctx = new SpongeContextDataContext(GetSpongeUrl());
+
+                return _ctx;
+            }
+        }
         public static SPWeb GetSpongeWeb()
         {
             using (var ca = SPWebManager.GetCentralAdminWeb())
                 return ca.Webs[Constants.SPONGE_WEB_URL];
+        }
+
+        public static string GetSpongeUrl()
+        {
+            using (var ca = SPWebManager.GetCentralAdminWeb())
+                return ca.Url + "/" + Constants.SPONGE_WEB_URL;
         }
     }
 }
