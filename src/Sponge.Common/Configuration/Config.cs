6885b06aa5063553159f67a3a8e88f14453473d8
int32at <admin@int32.at>
2013-07-19 17:38:18 +0200

    added persisted config

---
+++ src/Sponge.Common/Configuration/Config.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Administration;
+
+namespace Sponge.Common.Configuration
+{
+    public class Config : SPPersistedObject
+    {
+        [Persisted]
+        private string clientId;
+
+        public Config(string name,  SPPersistedObject parent)
+        {
+            
+        }
+
+        //public Config(string name, SPPersistedObject parent)
+        //    : base(name, parent)
+        //{
+        //}
+    }
+}
+;
