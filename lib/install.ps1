e02c4fdc55fcafbce048433aaf8d35075451edb3
int32at <admin@int32.at>
2013-07-20 13:55:58 +0200

    added install script to lib that install sponge on SP

---
+++ lib/install.ps1
@@ -0,0 +1,39 @@
+if ((Get-PSSnapin | ? { $_.Name -eq "Microsoft.SharePoint.PowerShell" }) -eq $null) {
+    Add-PSSnapin Microsoft.SharePoint.PowerShell
+}
+write-host "SharePoint Snapin loaded"
+
+function wait4timer($wspFileName) 
+{
+    $solution = Get-SPSolution | where-object {$_.Name -eq $wspFileName}
+    if ($solution -ne $null) 
+    {
+        $counter = 1   
+        
+        Write-Host "Waiting to finish soultion timer job"
+        while( ($solution.JobExists -eq $true ) -and ( $counter -lt 50 ) ) 
+        {   
+            Write-Host "Please wait..."
+            sleep 2
+            $counter++   
+        }
+        
+        Write-Host "Finished the solution timer job"         
+    }
+}
+
+###############################################################################################
+
+$wspFileName = "Sponge.wsp"
+
+$path = Join-Path $(get-location) $wspFileName
+
+write-host "Adding solution $wspFileName to the farm"
+add-spsolution -literalpath "$path"
+
+write-host "Deploying solution $wspFileName"
+install-spsolution -Identity "$wspFileName" -gacdeployment
+
+wait4timer($wspFileName)
+write-host "Done adding"
+
