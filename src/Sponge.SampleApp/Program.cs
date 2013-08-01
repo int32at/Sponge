683a0955651e5d0005e1ffcc09c2d597180065d4
int32at <admin@int32.at>
2013-08-01 12:59:19 +0200

    added samples project

---
+++ src/Sponge.SampleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sponge.Client.Logging;
+
+namespace Sponge.SampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var log = ClientLogManager.GetOffline("NLog.config", "Sponge.SampleApp.Program");
+
+            for (int i = 0; i < 1000; i++)
+            {
+                try
+                {
+                    var a = 0;
+                    var x = 3 / a;
+                }
+                catch (Exception ex)
+                {
+                    log.FatalException("Unexpected Exception occured.", ex);
+                }
+            }
+        }
+    }
+}
